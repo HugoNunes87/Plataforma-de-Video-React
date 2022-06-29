db621078a364a1405ae00b423ed2662668c19220
HugoNunes87 <hugolima87@hotmail.com>
2022-06-28 21:46:22 -0300

    App

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -27,15 +27,15 @@ export function Home() {
 
     
     return (
-        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center ">
-            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+        <div className="flex flex-col min-h-screen bg-blur bg-cover bg-no-repeat items-center">
+            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto home">
                 <div className="max-w-[640px] logo">
                     <Logo />
 
-                    <h1 className="mt-8 text-[2.5rem] leading-tight">
+                    <h1 className="mt-8 text-[2.5rem] leading-tight title">
                         Construa uma <strong className="text-blue-500 ">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
                     </h1>
-                    <p className="mt-4 text-gray-200 leading-relaxed" >
+                    <p className="mt-4 text-gray-200 leading-relaxed subtitle" >
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
