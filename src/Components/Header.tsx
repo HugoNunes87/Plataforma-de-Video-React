f391586f0b9ec612b88bba69777bd34da9c6d41b
HugoNunes87 <hugolima87@hotmail.com>
2022-07-02 13:31:27 -0300

    menu mobile_e_responsividade

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,33 +1,44 @@
 import { Logo } from "./Logo";
 import { useState } from 'react';
+import { Lesson } from "./Lesson";
+
+import { useGetLessonsQuery } from "../graphql/generated";
 
 
 export function Header() {
+    const { data } = useGetLessonsQuery()
 
     const [active, setMode] = useState(false);
     const ToggleMode = () => {
         setMode(!active)
     }
 
-
     return (
         <header className="w-full py-5 flex items-center justify-between bg-gray-700 border-b border-gray-600">
             <div className="ml-6">
                 <Logo />
             </div>
-            <div className={active ? 'icon iconActive' : 'icon'} onClick={ToggleMode}>
-                <div className='hamburguer hamburguerIcon'></div>
-            </div>
-
-            <div className={active ? 'menu menuOpen' : "menu menuClose"}>
-                <div className='list'>
-                    <ul className='listIems'>
-                        <li>ola</li>
-                        <li>mundo</li>
-                        <li>deu </li>
-                    </ul>
+            <nav>
+                <div className={active ? 'mobile-menu active' : 'mobile-menu'} onClick={ToggleMode}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
                 </div>
-            </div>
+                
+                <div className={active ? 'nav-list active' : 'nav-list'} onClick={ToggleMode}>
+                    {data?.lessons.map(lesson => {
+                        return (
+                            <Lesson
+                                key={lesson.id}
+                                title={lesson.title}
+                                slug={lesson.slug}
+                                availableAt={new Date(lesson.availableAt)}
+                                type={lesson.lessonType}
+                            />
+                        )
+                    })}
+                </div>
+            </nav>
         </header>
     )
 }
